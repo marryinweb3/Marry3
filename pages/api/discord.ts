643ed6d1b88712412534144a5dfca044cc243d2a
yootou <undefined@undefined.com>
2022-07-04 13:36:46 +0800

    feat: check discord marry

--- pages/api/discord.ts
+++ pages/api/discord.ts
@@ -7,6 +7,9 @@ import {
   ButtonStyleTypes,
 } from "discord-interactions";
 import { verifyMarried } from "../../lib/verify";
+import { HasGuildCommands, CHECK_COMMAND } from "../../lib/commands";
+
+HasGuildCommands(process.env.APP_ID, process.env.GUILD_ID, [CHECK_COMMAND]);
 
 const handler: NextApiHandler = async (req, res) => {
   // Interaction type and data
@@ -26,17 +29,6 @@ const handler: NextApiHandler = async (req, res) => {
   if (type === InteractionType.APPLICATION_COMMAND) {
     const { name } = data;
 
-    // "test" guild command
-    if (name === "test") {
-      // Send a message into the channel where command was triggered from
-      return res.send({
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data: {
-          // Fetches a random emoji to send from a helper function
-          content: "hello marry3!",
-        },
-      });
-    }
     // "challenge" guild command
     if (name === "check" && id) {
       const userId = req.body.member.user.id;
