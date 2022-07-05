e2d550521d6fb5fc170564662fbcefd0b34150bd
yootou <undefined@undefined.com>
2022-07-05 10:06:14 +0800

    feat: ga

--- components/main/common/head.com.tsx
+++ components/main/common/head.com.tsx
@@ -17,13 +17,18 @@ export function Web3Head() {
         type="text/css"
         href="https://csshake.surge.sh/csshake.min.css"
       ></link>
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-7G9N2XCM10"
+      ></script>
       <Script strategy="lazyOnload">
-        {` (function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-NV95S3L');
-        `}
+        {` 
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-7G9N2XCM10');`}
       </Script>
     </Head>
   );
